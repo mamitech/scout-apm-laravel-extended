7a284a0cbbc1d7089c40f32cd8b06ed008ec35d4
Aulia Rachmawan <mawan@mamiteam.com>
2023-10-06 16:07:53 +0700

    typo space

--- config/scout-apm-laravel-extended.php
+++ config/scout-apm-laravel-extended.php
@@ -2,6 +2,6 @@
 
 // config for mamitech/scout-apm-laravel-extended
 return [
-    'sampling_per' =>(int) env('SCOUT_SAMPLING_PER', 10),
+    'sampling_per' => (int) env('SCOUT_SAMPLING_PER', 10),
     'custom_context_enabled' => env('SCOUT_CUSTOM_CONTEXT_ENABLED', true),
 ];
